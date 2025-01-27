dfc9e9f9c929ba04bd77b9919aaf5161b96844dc
parham <parhamramin1386@gmail.com>
2025-01-27 19:44:50 +0330

    part#7

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+  
+
+  const adduserbutton = document.getElementById('add_user_button')
+  adduserbutton.addEventListener('click', ()=>{
+    document.querySelector('.add_user_modal').classList.add('show')
+    document.querySelector('.add_user_modal_back').classList.remove('disnone')
+
+  });
+
+
+  document.querySelector('.add_user_modal_back').addEventListener('click', (e)=>{
+    document.querySelector('.add_user_modal').classList.remove('show')
+    e.target.classList.add('dis-none')
+ 
+   })
   });
